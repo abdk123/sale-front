1966da234eace4e99838e25238ac9003986e0909
MohamadTonbary <ttmohamad299@gmail.com>
2024-07-16 10:23:30 +0300

    views

--- src/app/vouchers/customer-voucher/edit-customer-voucher/edit-customer-voucher.component.ts
+++ src/app/vouchers/customer-voucher/edit-customer-voucher/edit-customer-voucher.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, EventEmitter, Injector, Output } from '@angular/core';
+import { AppComponentBase } from '@shared/app-component-base';
+import { UpdateCustomerVoucherDto, DropdownDto, CustomerVoucherServiceProxy, CustomerServiceProxy } from '@shared/service-proxies/service-proxies';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { finalize } from 'rxjs';
 
 @Component({
-  selector: 'edit-customer-voucher',
-  templateUrl: './edit-customer-voucher.component.html',
-  styleUrls: ['./edit-customer-voucher.component.scss']
+  selector: "edit-customer-voucher",
+  templateUrl: "./edit-customer-voucher.component.html",
 })
-export class EditCustomerVoucherComponent {
+export class EditCustomerVoucherComponent extends AppComponentBase {
+  saving = false;
+  customerVoucher = new UpdateCustomerVoucherDto();
+  id: number;
+  @Output() onSave = new EventEmitter<any>();
+  customers: DropdownDto[] = [];
+  voucherDate: string;
 
+  types = [
+    {
+      id: 0,
+      name: this.l("Spend"),
+    },
+    {
+      id: 1,
+      name: this.l("Receive"),
+    },
+  ];
+
+  currencies = [
+    {
+      id: 0,
+      name: this.l("Dinar"),
+    },
+    {
+      id: 1,
+      name: this.l("Dollar"),
+    },
+  ];
+
+  constructor(
+    injector: Injector,
+    private _customerVoucherService: CustomerVoucherServiceProxy,
+    private _customerService: CustomerServiceProxy,
+    public bsModalRef: BsModalRef
+  ) {
+    super(injector);
+  }
+
+  ngOnInit(): void {
+    this.initialCustomerVoucher();
+    this.initialTransportCompanies();
+  }
+
+  initialCustomerVoucher() {
+    this._customerVoucherService
+      .getForEdit(this.id)
+      .subscribe((result: UpdateCustomerVoucherDto) => {
+        this.customerVoucher = result;
+
+        this.voucherDate = new DatePipe("en-US").transform(
+          this.customerVoucher.voucherDate,
+          "yyyy-MM-dd"
+        );
+      });
+  }
+
+  initialTransportCompanies() {
+    this._customerService.getForDropdown().subscribe((result) => {
+      this.customers = result;
+    });
+  }
+
+  save(): void {
+    this.saving = true;
+    this.customerVoucher.voucherDate = this.voucherDate;
+    this._customerVoucherService
+      .update(this.customerVoucher)
+      .pipe(
+        finalize(() => {
+          this.saving = false;
+        })
+      )
+      .subscribe((response: any) => {
+        response;
+        this.notify.info(this.l("SavedSuccessfully"));
+        this.bsModalRef.hide();
+        this.onSave.emit();
+      });
+  }
 }
--- src/app/vouchers/customer-voucher/view-customer-voucher/view-customer-voucher.component.ts
+++ src/app/vouchers/customer-voucher/view-customer-voucher/view-customer-voucher.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { AppComponentBase } from '@shared/app-component-base';
+import { CustomerDto, CustomerServiceProxy, CustomerVoucherDto, CustomerVoucherServiceProxy } from '@shared/service-proxies/service-proxies';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
-  selector: 'view-customer-voucher',
-  templateUrl: './view-customer-voucher.component.html',
-  styleUrls: ['./view-customer-voucher.component.scss']
+  selector: "view-customer-voucher",
+  templateUrl: "./view-customer-voucher.component.html",
 })
-export class ViewCustomerVoucherComponent {
+export class ViewCustomerVoucherComponent
+  extends AppComponentBase
+  implements OnInit
+{
+  data = new CustomerVoucherDto();
+  customer = new CustomerDto();
+  id: number;
+  editable: true;
 
+  types = [
+    {
+      id: 0,
+      name: this.l("Spend"),
+    },
+    {
+      id: 1,
+      name: this.l("Receive"),
+    },
+  ];
+
+  currencies = [
+    {
+      id: 0,
+      name: this.l("Dinar"),
+    },
+    {
+      id: 1,
+      name: this.l("Dollar"),
+    },
+  ];
+
+  constructor(
+    injector: Injector,
+    public bsModalRef: BsModalRef,
+    private _customerVoucherService: CustomerVoucherServiceProxy,
+    private _customerService: CustomerServiceProxy
+  ) {
+    super(injector);
+  }
+
+  ngOnInit(): void {
+    this.initCustomerVoucher();
+  }
+
+  initCustomerVoucher() {
+    this._customerVoucherService.get(this.id).subscribe((response) => {
+      this.data = response;
+      this.initialCustomer(this.data.customerId);
+      this.initialType(response.voucherType);
+      this.initialCurrency(response.currency);
+    });
+  }
+
+  initialCustomer(customerId: number) {
+    this._customerService.get(customerId).subscribe((result) => {
+      this.customer = result;
+    });
+  }
+  type: string;
+  currency: string;
+  initialType(id) {
+    this.type = this.types.find((x) => x.id == id).name;
+  }
+
+  initialCurrency(id) {
+    this.currency = this.currencies.find((x) => x.id == id).name;
+  }
 }
+
